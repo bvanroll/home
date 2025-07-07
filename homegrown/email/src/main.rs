78f67a8d255962b4824abee7e559cb2dd17825a3
bvanroll <beppe.vanrolleghem@gmail.com>
2025-07-07 23:41:43 +0200

    new entries

--- homegrown/email/src/main.rs
+++ homegrown/email/src/main.rs
@@ -3,11 +3,11 @@ use std::collections::HashMap;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //getting the list of channels with id's first 
-    let resp = reqwest::get("https://raw.githubusercontent.com/bvanroll/home/static/yters.json")
+    //https://raw.githubusercontent.com/bvanroll/home/refs/heads/master/static/yters.json
+    let resp = reqwest::get("https://raw.githubusercontent.com/bvanroll/home/refs/heads/master/static/yters.json")
         .await?
         .json::<HashMap<String, String>>()
         .await?;
     println!("{resp:#?}");
     Ok(())
-
 }
