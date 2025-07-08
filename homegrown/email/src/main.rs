781bbe9fda7f9503ac7a9cd0eb00633b9bc7a860
bvanroll <beppe.vanrolleghem@gmail.com>
2025-07-08 10:58:19 +0200

    idk

--- homegrown/email/src/main.rs
+++ homegrown/email/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 
 struct Channel {
     id: String,
@@ -8,15 +6,15 @@ struct Channel {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     let channellist = "https://raw.githubusercontent.com/bvanroll/home/refs/heads/master/static/yters.yaml";
     //getting the list of channels with id's first 
     //
     //https://raw.githubusercontent.com/bvanroll/home/refs/heads/master/static/yters.json
     let resp = reqwest::get(channellist)
-        .await?
+        .await.
         .text()
-        .await?;
-    let channels: Vec<Channel> =serde_yaml_ng::from_str(&resp)?;
+        .await;
+    let channels: Vec<Channel> =serde_yaml_ng::from_str(&resp);
 
 }
