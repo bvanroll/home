e10c3bcff21a798edca12f16204ed3c2286e8e14
bvanroll <beppe.vanrolleghem@gmail.com>
2025-07-13 12:34:52 +0200

    oops, better like this

--- homegrown/email/src/main.rs
+++ homegrown/email/src/main.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize; 
 use std::env;
+use chrono;
+use dotenv::dotenv;
 
 #[derive(Deserialize)]
 struct Channel {
@@ -8,17 +10,26 @@ struct Channel {
     kind: String,
 }
 
+struct Videos {
+    id: String,
+    name: String,
+}
+
 #[tokio::main]
 async fn main() {
+    dotenv.ok();
     let channellist = "https://raw.githubusercontent.com/bvanroll/home/refs/heads/master/static/yters.json";
     let resp = reqwest::get(channellist)//.await.unwrap().text().await;
                     .await.unwrap()
                     .text().await.unwrap();
     let channels = serde_json::from_str::<Vec<Channel>>(&resp).unwrap();
     let apikey = env::var("APIKEY").unwrap();
+//    let lastMonday = 
     for i in channels {
         let id = i.id;
-        let videos_request = format!("https://www.googleapis.com/youtube/v3/channels?id={id}&key={apikey}&part=contentDetails");
+        
+        let videos_request = format!("https://www.googleapis.com/youtube/v3/search/?channelId={id}&part=snippet,id&order=date&publishedAfter={date}&key={apikey}");
+        //let videos_request = format!("https://www.googleapis.com/youtube/v3/channels?id={id}&key={apikey}&part=contentDetails");
         println!("{}", videos_request);
     }
 
